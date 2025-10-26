a37d8a8da64821e499d726088a6fb6917c401bef
Themightyfirefly <niklschlueter@gmail.com>
2025-10-26 19:19:08 +0000

    changed hyphens to underscores in package names

---
+++ ch2-1-creating-nodes/ros2-node-test-workspace/build/ros2_sample_package_cmake/CMakeFiles/ros2_sample_package_cmake_uninstall.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for custom commands dependencies management for ros2_sample_package_cmake_uninstall.
---
+++ ch2-1-creating-nodes/ros2-node-test-workspace/src/ros2_sample_package_python/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'ros2_sample_package_python'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='niklschlueter@gmail.com',
+    description='An example package for the RISE ROS tutorial',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'test_our_node = ros2_sample_package_python.sample_node:main',
+        ],
+    },
+)
